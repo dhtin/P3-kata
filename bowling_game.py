72919d0910558820c1c08aa44b6c2f69898153a7
dhtin <markjo1997@gmail.com>
2025-09-01 22:46:36 -0500

    Implement spare bonus

--- bowling_game.py
+++ bowling_game.py
@@ -1,6 +1,7 @@
 class Game:
 
     def __init__(self):
+        self.rolls = []
         self.total_score = 0
 
     def roll(self, pins: int):
@@ -10,7 +11,15 @@ class Game:
         if pins < 0 or pins > 10:
             raise ValueError("Invalid number of pins")
         
+        self.rolls.append(pins)
         self.total_score += pins
+        self._handle_spare_bonus()
+
+    def _handle_spare_bonus(self):
+        if len(self.rolls) > 2:
+            if self.rolls[-3] + self.rolls[-2] == 10:
+                    self.total_score += self.rolls[-1]
+        
 
     def score(self) -> int:
         return self.total_score
