7f9980460d7bbb4460dda25e5b179816a6243477
dhtin <markjo1997@gmail.com>
2025-09-01 21:59:19 -0500

    Define roll function and refactor

--- bowling_game.py
+++ bowling_game.py
@@ -1,4 +1,10 @@
 class Game:
 
+    def __init__(self):
+        self.total_score = 0
+
+    def roll(self, pins: int):
+        self.total_score += pins
+
     def score(self) -> int:
-        return 0
+        return self.total_score
