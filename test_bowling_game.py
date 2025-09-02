6a9486ed9cc49d22447bab001b429f63ebdf730b
dhtin <markjo1997@gmail.com>
2025-09-01 21:20:40 -0500

    Add unit test main

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -6,3 +6,6 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         output = something()
         self.assertTrue(output)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
