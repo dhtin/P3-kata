8614d72c884da88855217eba7cdb68f2159385d6
dhtin <markjo1997@gmail.com>
2025-09-01 22:11:47 -0500

    Add check that pins is int

--- bowling_game.py
+++ bowling_game.py
@@ -4,8 +4,12 @@ class Game:
         self.total_score = 0
 
     def roll(self, pins: int):
+        if not isinstance(pins, int):
+            raise TypeError("Pins must be an int")
+        
         if pins < 0 or pins > 10:
             raise ValueError("Invalid number of pins")
+        
         self.total_score += pins
 
     def score(self) -> int:
