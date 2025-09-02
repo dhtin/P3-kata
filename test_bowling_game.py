095ee788c1233d8d6f5707abc69f3ab8bb647e5d
dhtin <markjo1997@gmail.com>
2025-09-01 21:42:50 -0500

    Cannibalize test_something to add test_starting_score

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -3,10 +3,10 @@ from bowling_game import Game
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_starting_score(self):
         bowling_game = Game()
-        output = bowling_game.something()
-        self.assertTrue(output)  # add assertion here
+        output = bowling_game.score()
+        self.assertEqual(0, output) 
 
 if __name__ == '__main__':
     unittest.main()
