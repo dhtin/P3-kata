fdef13c5171ee0bcfadc7e7c3fc88108bcb4c224
dhtin <markjo1997@gmail.com>
2025-09-01 22:05:21 -0500

    Add invalid pin number error

--- bowling_game.py
+++ bowling_game.py
@@ -4,6 +4,8 @@ class Game:
         self.total_score = 0
 
     def roll(self, pins: int):
+        if pins < 0 or pins > 10:
+            raise ValueError("Invalid number of pins")
         self.total_score += pins
 
     def score(self) -> int:
